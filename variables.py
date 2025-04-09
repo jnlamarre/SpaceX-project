0126fa6afac318fef9ae83b8094f384887c95482
Jean-Nicolas <jeannicolas.lamarre@gmail.com>
2025-04-09 21:21:56 +0200

    Add variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Number of launches
+launch_count = 10
+
+# Name of the rocket
+rocket_name = "Falcon 9"
+
+# Success rate of launches
+success_rate = 0.95
+
+# Print the values to the console
+print("Rocket:", rocket_name)
+print("Launches:", launch_count)
+print("Success rate:", success_rate)
